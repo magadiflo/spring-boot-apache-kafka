5db000cdc41d62b75ddeeb98f5041a306b98fd8f
Martín <magadiflo@gmail.com>
2023-11-28 12:54:14 -0500

    Adjust the Consumer and test the changes

--- kafka-demo/src/main/java/dev/magadiflo/kafka/app/consumer/KafkaConsumer.java
+++ kafka-demo/src/main/java/dev/magadiflo/kafka/app/consumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package dev.magadiflo.kafka.app.consumer;
 
+import dev.magadiflo.kafka.app.payload.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,15 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumer {
 
     //groupId = "myGroup", el que configuramos en el application.yml
-    @KafkaListener(topics = "magadiflo", groupId = "myGroup")
+    //@KafkaListener(topics = "magadiflo", groupId = "myGroup")
     public void consumeMessage(String message) {
-      log.info("Consumiendo mensaje desde el topic magadiflo: {}", message);
+        log.info("Consumiendo mensaje desde el topic magadiflo: {}", message);
+    }
+
+
+    @KafkaListener(topics = "magadiflo", groupId = "myGroup")
+    public void consumeJsonMessage(Student student) {
+        log.info("Consumiendo student desde el topic magadiflo: {}", student);
     }
 
 }
--- kafka-demo/src/main/java/dev/magadiflo/kafka/app/payload/Student.java
+++ kafka-demo/src/main/java/dev/magadiflo/kafka/app/payload/Student.java
@@ -2,7 +2,9 @@ package dev.magadiflo.kafka.app.payload;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 @Getter
 @Setter
 public class Student {
